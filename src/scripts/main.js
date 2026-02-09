ed251ac4eb9fdc45174cddbf68af31f25e3b2118
Oleksii <olexijukraina@gmail.com>
2026-02-09 21:16:48 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const treeItems = document.querySelectorAll('li');
+
+treeItems.forEach((li) => {
+  const sublist = li.querySelector('ul');
+
+  if (!sublist) {
+    return;
+  }
+
+  const textNode = li.firstChild;
+
+  if (!textNode || textNode.nodeType !== 3) {
+    return;
+  }
+
+  const span = document.createElement('span');
+
+  span.textContent = textNode.textContent.trim();
+  li.insertBefore(span, textNode);
+  li.removeChild(textNode);
+
+  sublist.hidden = false;
+
+  span.addEventListener('click', () => {
+    sublist.hidden = !sublist.hidden;
+  });
+});
